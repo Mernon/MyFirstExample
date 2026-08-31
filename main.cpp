d30573caeadf1c13e470a93b89d9b82f95fedef3
Tyler Wagenman <wagenman01@yahoo.com>
2026-08-31 11:55:10 -0700

    Fixing NaN error for division operations.

--- main.cpp
+++ main.cpp
@@ -16,8 +16,18 @@ int main()
     cout << "Addition: " << x + y << endl;
     cout << "Subtraction: " << x - y << endl;
     cout << "Multiplication: " << x * y << endl;
-    cout << "Division: " << x / y << endl;
-    cout << "Remainder: " << x % y << endl;
+    if (y == 0)
+    {
+    	cout << "Diving by zero is not a number." << endl;
+    }
+    else
+    {
+    	cout << "Division: " << x / y << endl;
+	cout << "Remainder: " << x % y << endl;
+    }
+
+    //cout << "Division: " << x / y << endl;
+    //cout << "Remainder: " << x % y << endl;
     cout << "Square Root: " << sqrt(x) << endl;
     cout << "Square: " << pow(x, y) << endl;
 
